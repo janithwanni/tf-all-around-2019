9765e057e5dd65ab8e3352cb02a674e44b5040bd
Janith Wanniarachchi <janithcwanni@gmail.com>
2019-08-18 11:28:35 +0530

    cleanup

--- index.js
+++ index.js
@@ -12,38 +12,29 @@ async function app(){
 		},
 		configuredColumnsOnly: true
 	});
-	console.log("testing");
+	const model = await use.load();
+	console.log("Loaded Model");
+	console.log("Features and Target");
 	console.log(await dataset.columnNames());
-	await dataset.take(3).forEachAsync(e=>console.log(e));
-	console.log("flat dataset");
 	const flatdataset = dataset.map(({xs, ys}) =>
       		{
         	return {xs:Object.values(xs), ys:Object.values(ys)};
-		});
-	await flatdataset.take(3).forEachAsync(e=>console.log(e));
-
-	const inputs = flatdataset.map(values => { return values.xs;});
-	//const labels = flatdataset.map(values => { return values.ys;});
-	console.log(inputs);
-	//const labelsDataset = tf.data.array(labels);
+	});
+	console.log("Flattenned dataset");
 	
-	const use_model = await use.load();
-	//const embeddings = await getEmbeddings(inputs,use_model);
-	//console.log("embeddings made with shape");
+	const embedDataset = await flatdataset.mapAsync(value => { return getEmbeddings(value,model);});
+	console.log("Embeded Dataset");
+	await embedDataset.take(3).forEachAsync(e=>console.log(e));
 	
-	//console.log(embeddings.print(true));
-	//const inputsDataset = tf.data.array(embeddings);
-	//console.log(inputsDataset);
-	// const Dataset = tf.data.zip(inputsDataset,labelsDataset);
-	// await Dataset.take(3).forEachAsync(e=>console.log(e));
 	
 }
-async function getEmbeddings(inputs,model){
-	return await model.embed(inputs).then(embeddings => {
-		return new Promise((resolve,reject) => {
-			resolve(embeddings);
-		});
+async function getEmbeddings(data,model){
+	const embeds = await model.embed(data.xs);
+	const embeds_array = await embeds.array();
+	return new Promise((resolve,reject) =>{
+		resolve({xs:embeds_array,ys:data.ys});
 	});
+	
 }
 function createModel(){
 	const model = tf.sequential();
@@ -52,3 +43,5 @@ function createModel(){
 	return model;
 }
 app();
+
+
